d102dca0afa2ea72bf0483128fc2a447aef041fc
Emil Nicolaie Perhinschi <emilper@gmail.com>
2018-09-16 02:28:28 +0300

    changed class Tap to struct Tap; skipping tests; better messages; improved debugging

--- source/zug/tap.d
+++ source/zug/tap.d
@@ -21,30 +21,65 @@ struct TapData {
 }
 
 
-class Tap {
-
-    private bool print_each_result = true;
+struct Tap {
     private int planned_tests;
-    private TapData[] results;
+    private TapData[] tests_data;
     private int tests_count = 0;
-    private string cache = "";
+    
+    private string cache = ""; // cache debug output here ... probably
+    
     private bool debug_enabled = false;
+    private bool print_messages = true;
 
-    this() {
-        this.debug_enabled = std.process.environment.get("DEBUG") == "1" ? true : false;
+    // skip tests and add them to skipped_tests until true
+    private bool skipping = false;
+    private int skipped_tests;
+
+    this(bool verbose) {
+        this.print_messages = verbose;
+        if (this.verbose()) {
+            this.debug_enabled = 
+                std.process.environment.get("DEBUG") == "1" ? true : false;
+        }
     }
 
-    void verbose(bool verbose) { this.print_each_result = verbose; }
-    bool verbose()             { return this.print_each_result; }
+    void verbose(bool verbose) { this.print_messages = verbose; }
+    bool verbose()             { return this.print_messages; }
 
-    void plan(int plan )       { this.planned_tests = plan; }
-    int plan()                 { return this.planned_tests ? this.planned_tests : 0; }
+    void write(string[] message ... ) {
+        if (this.verbose()) {
+            writeln(message.join(" "));
+        }
+    }
+
+
+    void warn(string[] message ... ) {
+        if(this.debug_enabled) {
+            stderr.writeln(message.join(" "));
+        }
+    }
+
+    void plan(int plan ) { this.planned_tests = plan; }
+    int plan() { return this.planned_tests ? this.planned_tests : 0; }
+
+    TapData[] results() { return this.tests_data; }
+
+    void add_result(bool success, string message) {
+        this.tests_count++;
+        this.write(
+            (success ? "ok" : "not ok"),
+            to!string(this.tests_count),
+            message
+        );
+        
+        this.tests_data ~= TapData(TapDataType.test_result, success, message);
+    }
 
     bool done_testing() {
         string[string] summary;
         int tests_passed = 0;
         int tests_failed = 0;
-        foreach (TapData result; this.results) {
+        foreach (TapData result; this.tests_data) {
             if(result.data_type != TapDataType.test_result) {
                 continue;
             }
@@ -57,52 +92,55 @@ class Tap {
             }
         }
 
-        if (this.debug_enabled) {
-            writeln(tests_passed, " tests passed; ", tests_failed, " tests failed");
-            writeln("Planned ", this.planned_tests,  "; completed ", this.tests_count);
-        }
+        this.write(to!string(tests_passed), "tests passed;", 
+            to!string(tests_failed), "tests failed");
+        this.write("Planned:", to!string(this.planned_tests),
+            "; completed:", to!string(this.tests_count),
+            "; skipped:", to!string(this.skipped_tests)
+        );
 
-        return this.planned_tests == tests_passed;
-    }
-    
-    void add_result(bool success, string message) {
-        this.tests_count++;
-        if(this.verbose) {
-            writeln( ( success ? "ok" : "not ok")
-                    ~ " " ~ to!string(this.tests_count) ~ " " ~ message);
-        }
-        this.results ~= TapData(TapDataType.test_result, success, message);
+        return tests_failed == 0;
     }
     
     void add_diagnostic(string message) {
-        if(this.verbose) {
-            auto lines = splitLines(message).map!(a => "  #" ~ stripRight(a)).join("\n");
-            writeln( "  #DIAGNOSTIC: " ~ "\n" ~ lines );
-        }
-        
-        this.results ~= TapData(TapDataType.diagnostic, true, message);
+        auto lines = splitLines(message)
+            .map!(a => "  #" ~ stripRight(a))
+            .join("\n");
+
+        this.write("  #DIAGNOSTIC: ", "\n", lines );
+        this.tests_data ~= TapData(TapDataType.diagnostic, true, message);
     }
     
     void add_note(string message) {
-        if(this.verbose) {
-            writeln( "#NOTE: " ~ " " ~ message);
-        }
-        this.results ~= TapData(TapDataType.note, true, message);
+        this.write( "#NOTE: ", message);
+        this.tests_data ~= TapData(TapDataType.note, true, message);
     }
 
+    bool ok(bool delegate () test, string message) {
+        if (this.skipping) {
+            this.skipped_tests += 1;
+            return true;
+        }
 
-    void ok(bool delegate () test, string message) {
+        bool result;
         try {
-            bool result = test();
-            this.add_result(result, message);
+            result = test();
         } catch (Exception e) {
+            result = false;
             this.add_diagnostic(e.msg);
-            this.add_result(false, message);
         }
+        this.add_result(result, message);
+        return result;
     }
 
-    void ok(bool is_true, string message ) {
+    bool ok(bool is_true, string message ) {
+        if (this.skipping) {
+            this.skipped_tests += 1;
+            return true;
+        }
+
         this.add_result(is_true, message);
+        return is_true;
     }
 
     void do_debug(string message) {
@@ -111,26 +149,51 @@ class Tap {
         }
     }
 
-    void skip() {
-        write_debug("TODO");
+    void skip(string message) {
+        this.skipping = true;
+        this.write("# skipping tests: ", message);
     }
     
+    void resume(string message) {
+        this.skipping = false;
+        this.write("# resuming tests: ", message);
+    }
 
     void start_subtest() {
-        write_debug("TODO");
+        this.warn("TODO start_subtest()");
     }
 }
 
 unittest {
-    auto tap = new Tap();
-    auto test_the_tap = new Tap();
-    tap.verbose(true);
-    tap.plan(10);
-    tap.ok(false, "should fail");
-    tap.ok(true, "should pass");
-    tap.ok(delegate bool () { return false; }, "should fail");
-    tap.ok(delegate bool () { return true; },  "should pass");
-    test_the_tap.ok(tap.done_testing(), "tap.done_testing should report errors");
+    bool talk_to_me = std.process.environment.get("DEBUG") == "1" ? true : false;
+    {
+        auto tap = Tap(talk_to_me);
+        assert(tap.verbose() == talk_to_me);
+        tap.plan(10);
+        assert(tap.plan() == 10);
+        assert(!tap.ok(false, "should fail"));
+        assert(tap.ok(true, "should pass"));
+        assert(!tap.ok(delegate bool () { return false; }, "should fail"));
+        assert(tap.ok(delegate bool () { return true; },  "should pass"));
+        assert(tap.results().length == 4);
+        assert(!tap.done_testing());
+    }
+
+    { // test skipping
+        auto tap = Tap(talk_to_me);
+        assert(tap.verbose() == talk_to_me);
+        assert(tap.ok(true, "should pass"));
+        assert(tap.ok(true, "should pass"));
+        tap.skip("skipping two out of six tests");
+        assert(tap.ok(true, "should pass"));
+        assert(tap.ok(true, "should pass"));
+        tap.resume("skipped two tests out of six, resuming");
+        assert(tap.ok(true, "should pass"));
+        assert(tap.ok(true, "should pass"));
+        assert(tap.results().length == 4);
+        assert(tap.skipped_tests == 2);
+        assert(tap.done_testing());
+    }
 }
 
 
